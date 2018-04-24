4caa9819f95647aa580e3b4822e010958ed274a6
撒汉西 <sahanxi@sahanxidemacbook-air.local>
2018-04-24 12:33:34 +0800

    Initial Commit

---
+++ SecondDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondDemo
+//
+//  Created by 撒汉西 on 24/04/2018.
+//  Copyright © 2018 撒汉西. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
